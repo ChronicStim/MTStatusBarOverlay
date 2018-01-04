34b90a0da0aa4fcb8c3df53961f03e8232d73388
Development <support@chronicstimulation.com>
2018-01-04 10:31:45 -0500

    Code cleanup on block calls.

--- MTStatusBarOverlay.m
+++ MTStatusBarOverlay.m
@@ -1846,6 +1846,6 @@ void mt_dispatch_sync_on_main_thread(dispatch_block_t block) {
     if ([NSThread isMainThread]) {
         block();
     } else {
-        dispatch_sync(dispatch_get_main_queue(), block);
+        runOnMainQueueWithoutDeadlocking(block);
     }
 }
